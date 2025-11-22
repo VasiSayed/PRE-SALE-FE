2deb032f61747acc9fce29ddd3fbc3f85df89daf
VasihSayed <vasisayed09421@gmail.com>
2025-11-22 16:55:50 +0530

    Working Site-Visit

--- src/features/Footer/Footer.jsx
+++ src/features/Footer/Footer.jsx
@@ -5,23 +5,31 @@ function Footer() {
     <footer className="footer">
       <div className="footer-content">
         <div className="footer-left">
-          <p className="footer-text">© 2025 LOTUS DEVELOPERS. All rights reserved.</p>
+          <p className="footer-text">
+            © 2025 SHREE RAM KRUSHNA DEVELOPERS. All rights reserved.
+          </p>
         </div>
-        
+
         <div className="footer-center">
-          <a href="#" className="footer-link">Privacy Policy</a>
+          <a href="#" className="footer-link">
+            Privacy Policy
+          </a>
           <span className="footer-divider">|</span>
-          <a href="#" className="footer-link">Terms of Service</a>
+          <a href="#" className="footer-link">
+            Terms of Service
+          </a>
           <span className="footer-divider">|</span>
-          <a href="#" className="footer-link">Support</a>
+          <a href="#" className="footer-link">
+            Support
+          </a>
         </div>
-        
+
         <div className="footer-right">
           <p className="footer-text">Version 1.0.0</p>
         </div>
       </div>
     </footer>
-  )
+  );
 }
 
 export default Footer
